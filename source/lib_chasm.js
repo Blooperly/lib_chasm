6b35482d81b54589791324ad6c68f3217e7c3feb
Blooperly <jbrownfisher@gmail.com>
2022-02-03 10:24:50 -0800

    Resource module progress

--- source/lib_chasm.js
+++ source/lib_chasm.js
@@ -1,8 +1,8 @@
 // Library Info - DO NOT MANUALLY EDIT, BUILT BY buildscript.js
 const _CHASM_VERSION_MAJOR = 0;
 const _CHASM_VERSION_MINOR = 0;
-const _CHASM_VERSION_BUILD = 58;
-const _CHASM_BUILD_TIME = new Date(1643751705917);
+const _CHASM_VERSION_BUILD = 70;
+const _CHASM_BUILD_TIME = new Date(1643885246619);
 
 // Resource Module
 	// Chasm Resources are the primary objects for storing player inventory and statistics. Resources are an
@@ -21,26 +21,32 @@ class chasm_resource {
 	option_unlocked = false;
 	option_cap = false;
 
-	constructor(name, id) {
+	// Constructor
+	constructor(name) {
 		this.name = name;
-		this.id = id;
 	}
 
-	// Add resource
-	gain(i) {
-		if (this.unlocked) {
-			this.current.plus(i);
-			this.alltime.plus(i);
+	// Gain resource
+	gain(amount) {
+		if (this.option_unlocked) {
+			this.current = this.current.plus(amount);
+			this.alltime = this.alltime.plus(amount);
+
+			if (this.option_cap && this.current.gt(this.cap)) {
+				this.current = this.cap;
+			}
+
 			return true;
 		}
-		
-		return false;
+		else {
+			return false;
+		}
 	}
 
 	// Spend resource
-	spend(i) {
-		if (this.current.gte(i)) {
-			this.current.minus(i);
+	spend(amount) {
+		if (this.current.gte(amount)) {
+			this.current = this.current.minus(amount);
 			return true;
 		}
 		else {
@@ -49,21 +55,24 @@ class chasm_resource {
 	}
 
 	// Print resource values. Useful for debugging, less useful for displaying in game.
+	// Format:
+	// 		[id] name: (current) (cap) (alltime)
 	toString() {
 		let string = 						"[" + this.id + "]";
 		string +=							" " + this.name + ":";
 		string +=							" (current = " + this.current + ")";
 		if (this.option_cap) string +=		" (cap = " + this.cap + ")";
+		string +=							" (alltime = " + this.alltime + ")";
 		return string;
 	}
 }
 
 // Progress Module
-	// Chasm Gates are 
+	// Chasm Gates are state
 
 // Task Module
 	// Chasm Tasks are state-driven actions. Tasks are gathered into "Task Stacks", which are groups of tasks
-	//that know each other's states. This can be used to implement mutually-exclusive task groups, task flows,
+	// that know each other's states. This can be used to implement mutually-exclusive task groups, task flows,
 	// and compounding tasks.
 class chasm_task {
 	// Task Info
--- test.js
+++ test.js
@@ -1,18 +1,39 @@
 console.log("Starting lib_chasm debug session:\n\tVersion: " + _CHASM_VERSION_MAJOR + "." + _CHASM_VERSION_MINOR + "." + _CHASM_VERSION_BUILD + "\n\tBuild Time: " + _CHASM_BUILD_TIME.toLocaleString());
 
 // Resource Initialization
-var wood = new chasm_resource("Wood", 1);
+var wood = new chasm_resource("Wood");
 wood.option_unlocked = true;
-var stone = new chasm_resource("Stone", 2);
+var stone = new chasm_resource("Stone");
 stone.option_unlocked = true;
-var iron = new chasm_resource("Iron", 3);
+var iron = new chasm_resource("Iron");
 iron.option_unlocked = true;
+iron.option_cap = true;
+iron.cap = 10;
 
 function chasm_test() {
+	draw_resources();
+
+	// Set breakpoint here to catch final state
+	console.log("\tClosing session...\n\n");
+}
+
+function draw_resources() {
 	document.getElementById("chasm_resource_wood").innerHTML = wood.toString();
 	document.getElementById("chasm_resource_stone").innerHTML = stone.toString();
 	document.getElementById("chasm_resource_iron").innerHTML = iron.toString();
+}
 
-	// Set breakpoint here to catch final state
-	console.log("\tClosing session...");
+function click_wood() {
+	wood.gain(1);
+	draw_resources();
+}
+
+function click_stone() {
+	stone.gain(1);
+	draw_resources();
+}
+
+function click_iron() {
+	iron.gain(1);
+	draw_resources();
 }
