d31dc7574237c181bcd0de9c9fc5cb27d611b551
Blooperly <jbrownfisher@gmail.com>
2022-02-08 18:25:10 -0800

    Prototype small resource + break out timing module

--- source/lib_chasm.js
+++ source/lib_chasm.js
@@ -1,47 +1,16 @@
 // Library Info - DO NOT MANUALLY EDIT, BUILT BY buildscript.js
 const _CHASM_VERSION_MAJOR = 0;
 const _CHASM_VERSION_MINOR = 0;
-const _CHASM_VERSION_BUILD = 91;
-const _CHASM_BUILD_TIME = new Date(1643941813587);
-
-// Timing Module
-let _CHASM_TIMING_LAST_TIMESTAMP = 0;
-let _CHASM_TIMING_FRACTIONAL_TICK = 0;
-let _CHASM_TIMING_CALLBACK = 0;
-let _CHASM_TIMING_TICK_TIME = 100;
-let _CHASM_TIMING_SCALAR = 0;
-
-function chasm_timing_init(callback, tick_time) {
-	_CHASM_TIMING_CALLBACK = callback;
-	_CHASM_TIMING_TICK_TIME = tick_time;
-	_CHASM_TIMING_SCALAR = tick_time / 1000;
-	window.requestAnimationFrame(_CHASM_TIMING_MODULE);
-}
-
-function _CHASM_TIMING_MODULE(timestamp) {
-	_CHASM_TIMING_FRACTIONAL_TICK += timestamp - _CHASM_TIMING_LAST_TIMESTAMP;
-	_CHASM_TIMING_LAST_TIMESTAMP = timestamp;
-	
-	while (_CHASM_TIMING_FRACTIONAL_TICK > _CHASM_TIMING_TICK_TIME ) {
-		_CHASM_TIMING_FRACTIONAL_TICK -= _CHASM_TIMING_TICK_TIME;
-		_CHASM_TIMING_CALLBACK(_CHASM_TIMING_SCALAR);
-	}
-
-	window.requestAnimationFrame(_CHASM_TIMING_MODULE);
-}
+const _CHASM_VERSION_BUILD = 94;
+const _CHASM_BUILD_TIME = new Date(1644373397895);
 
 // Resource Module
 	// Chasm Resources are the primary objects for storing player inventory and statistics. Resources are an
 	// abstract concept that represent anything the player can generate, collect, or spend.
-class chasm_resource {
+
+class _CHASM_RESOURCE_TEMPLATE {
 	// Resource Info
 	name = "";
-	id = 0;
-
-	// Resource Stats
-	current = new BigNumber(0);
-	cap = new BigNumber(0);
-	alltime = new BigNumber(0);
 
 	// Resource Options
 	option_unlocked = false;
@@ -51,6 +20,18 @@ class chasm_resource {
 	// Constructor
 	constructor(name) {
 		this.name = name;
+	}
+}
+
+class chasm_resource extends _CHASM_RESOURCE_TEMPLATE {
+	// Resource Stats
+	current = new BigNumber(0);
+	cap = new BigNumber(0);
+	alltime = new BigNumber(0);
+
+	// Constructor
+	constructor(name) {
+		super(name);
 	}
 
 	// Gain resource
@@ -85,8 +66,58 @@ class chasm_resource {
 	// Format:
 	// 		[id] name: (current) (cap) (alltime)
 	toString() {
-		let string = 						"[" + this.id + "]";
-		string +=							" " + this.name + ":";
+		let string = 						" " + this.name + ":";
+		string +=							" (current = " + this.current.toFixed(0) + ")";
+		if (this.option_cap) string +=		" (cap = " + this.cap.toFixed(0) + ")";
+		string +=							" (alltime = " + this.alltime.toFixed(0) + ")";
+		return string;
+	}
+}
+
+class chasm_resource_small extends _CHASM_RESOURCE_TEMPLATE {
+	// Resource Stats
+	current = 0;
+	cap = 0;
+	alltime = 0;
+
+	// Constructor
+	constructor(name) {
+		super(name);
+	}
+
+	// Gain resource
+	gain(amount) {
+		if (this.option_unlocked) {
+			this.current += amount;
+			this.alltime += amount;
+
+			if (this.option_cap && this.current > this.cap) {
+				this.current = this.cap;
+			}
+
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	// Spend resource
+	spend(amount) {
+		if (this.current >= amount) {
+			this.current -= amount;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	// Print resource values. Useful for debugging, less useful for displaying in game.
+	// Format:
+	// 		[id] name: (current) (cap) (alltime)
+	toString() {
+		let string = 						" " + this.name + ":";
 		string +=							" (current = " + this.current.toFixed(0) + ")";
 		if (this.option_cap) string +=		" (cap = " + this.cap.toFixed(0) + ")";
 		string +=							" (alltime = " + this.alltime.toFixed(0) + ")";
@@ -95,13 +126,10 @@ class chasm_resource {
 }
 
 // Price Module
-// returns price for bnNumber of resources
-function chasm_price_flat(bnCurrent, bnPriceBase, bnPriceChange, bnNumber) {
+
+// Returns price for one resource
+function chasm_price_flat(bnCurrent, bnPriceBase, bnPriceChange) {
 	return (bnPriceBase.plus(bnCurrent.times(bnPriceChange)));
-}
-
-function chasm_price_max_buyable_flat() {
-
 }
 
 // Progress Module
@@ -121,3 +149,6 @@ class chasm_task {
 		this.name
 	}
 }
+
+// Devtools Module
+// Tool for graphing exponential progress in relation to upgrades & time
--- test.js
+++ test.js
@@ -1,14 +1,13 @@
 console.log("Starting lib_chasm debug session:\n\tVersion: " + _CHASM_VERSION_MAJOR + "." + _CHASM_VERSION_MINOR + "." + _CHASM_VERSION_BUILD + "\n\tBuild Time: " + _CHASM_BUILD_TIME.toLocaleString());
 
 // Resource Initialization
-var wood = new chasm_resource("Wood");
-wood.option_unlocked = true;
-var stone = new chasm_resource("Stone");
-stone.option_unlocked = true;
-var iron = new chasm_resource("Iron");
-iron.option_unlocked = true;
-iron.option_cap = true;
-iron.cap = BigNumber(10);
+var resource = new chasm_resource("chasm_resource");
+resource.option_unlocked = true;
+resource.option_cap = true;
+resource.cap = BigNumber(10);
+
+var resource_small = new chasm_resource_small("chasm_resource_small");
+resource_small.option_unlocked = true;
 
 function chasm_test() {
 	draw_resources();
@@ -19,30 +18,26 @@ function chasm_test() {
 }
 
 function chasm_tick(scalar) {
-	wood.gain(BigNumber(1).times(scalar));
+	resource_small.gain(1 * scalar);
 	draw_resources(); // Move to animation loop later
 }
 
 function draw_resources() {
-	document.getElementById("chasm_resource_wood").innerHTML = wood.toString();
-	document.getElementById("chasm_resource_stone").innerHTML = stone.toString();
-	document.getElementById("chasm_resource_iron").innerHTML = iron.toString();
+	document.getElementById("chasm_resource").innerHTML = resource.toString();
+	document.getElementById("chasm_resource_small").innerHTML = resource_small.toString();
 }
 
-function click_wood() {
-	wood.gain(1);
+// Button handling
+function click_resource() {
+	resource.gain(1);
 }
 
-function click_stone() {
-	stone.gain(1);
-}
-
-function click_iron() {
-	iron.gain(1);
-}
-
-function click_iron_expand() {
-	if (iron.spend(10)){
-		iron.cap = iron.cap.plus(10);
+function click_resource_expand() {
+	if (resource.spend(10)) {
+		resource.cap = resource.cap.plus(10);
 	}
+}
+
+function click_resource_small() {
+	resource_small.gain(1);
 }
